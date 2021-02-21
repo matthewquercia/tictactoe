e54888fa429f5e2712d5abfdefe137ba5910e6dd
unknown <quercia.matt@gmail.com>
2021-02-21 14:16:01 -0500

    finalizing game

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -3,63 +3,76 @@ import './Styles.css'
 
 class TicTacToe extends React.Component {
     state = {
-        playersTurn: 'O',
+        playersTurn: 'X',
         board: [["","",""],["","",""],["","",""]],
-        turn: 1
+        turn: 1,
+        gameOver: false,
+        winner: ''
     }
 
-    ConsoleLog = (row,col) => {
-        let current = this.state.board;
+    populateBoard = (row,col) => {
+        if(this.state.gameOver === false){
+            let current = this.state.board;
 
-        if(this.state.playersTurn === 'O'){
-            if(!current[row][col]){
-                current[row][col] = 'X'
-                this.setState({board: current, playersTurn: 'X', turn: this.state.turn+1})
+            if(this.state.playersTurn === 'X'){
+                if(!current[row][col]){
+                    current[row][col] = 'X'
+                    this.setState({board: current, playersTurn: 'O', turn: this.state.turn+1})
+                }
+            } else {
+                if(!current[row][col]){
+                    current[row][col] = 'O'
+                    this.setState({board: current, playersTurn: 'X', turn: this.state.turn+1})
+                }
             }
-        } else {
-            if(!current[row][col]){
-                current[row][col] = 'O'
-                this.setState({board: current, playersTurn: 'O', turn: this.state.turn+1})
-            }
+    
+            if(this.state.turn > 4){
+                this.validateWinner(row,col, this.state.playersTurn);
+            } 
         }
-        this.validateWinner(row,col);
     }
 
-    validateWinner = (row,col) => {
+    validateWinner = (row,col, player) => {
         const checkRow = this.state.board[row].every(val => val === this.state.board[row][0]);
         const checkColumn = this.state.board.every(val => val[col] === this.state.board[row][col])
         const checkDiag1 = this.state.board.every((val, idx) => val[idx] === this.state.board[row][col]);
         const checkDiag2 = this.state.board.every((val, idx, arr) => val[arr.length - (1 + idx)] === this.state.board[row][col])
 
         if(checkRow === true || checkColumn === true || checkDiag1 === true || checkDiag2 === true){
-            console.log('Winner!!!!!!!!');
-        } else {
-            console.log('Game still in progress...');
+            this.endGame(player);
         }
+        else if(this.state.turn === 9){
+            this.setState({gameOver: true, winner: 'Draw!'})
+        }
+    }
+
+    endGame = (player) => {
+        this.setState({gameOver: true, winner: player + ' Wins!'})
     }
 
     render() {
         return (
-            <div>
-                <table>
+            <div className="center-div">
+                <table className="center-table">
                     <thead>
                         <tr>
-                            <td onClick={() => this.ConsoleLog(0,0)}>{this.state.board[0][0]}</td>
-                            <td onClick={() => this.ConsoleLog(0,1)}>{this.state.board[0][1]}</td>
-                            <td onClick={() => this.ConsoleLog(0,2)}>{this.state.board[0][2]}</td>
+                            <td onClick={() => this.populateBoard(0, 0)}>{this.state.board[0][0]}</td>
+                            <td onClick={() => this.populateBoard(0, 1)}>{this.state.board[0][1]}</td>
+                            <td onClick={() => this.populateBoard(0, 2)}>{this.state.board[0][2]}</td>
                         </tr>
                         <tr>
-                            <td onClick={() => this.ConsoleLog(1,0)}>{this.state.board[1][0]}</td>
-                            <td onClick={() => this.ConsoleLog(1,1)}>{this.state.board[1][1]}</td>
-                            <td onClick={() => this.ConsoleLog(1,2)}>{this.state.board[1][2]}</td>
+                            <td onClick={() => this.populateBoard(1, 0)}>{this.state.board[1][0]}</td>
+                            <td onClick={() => this.populateBoard(1, 1)}>{this.state.board[1][1]}</td>
+                            <td onClick={() => this.populateBoard(1, 2)}>{this.state.board[1][2]}</td>
                         </tr>
                         <tr>
-                            <td onClick={() => this.ConsoleLog(2,0)}>{this.state.board[2][0]}</td>
-                            <td onClick={() => this.ConsoleLog(2,1)}>{this.state.board[2][1]}</td>
-                            <td onClick={() => this.ConsoleLog(2,2)}>{this.state.board[2][2]}</td>
+                            <td onClick={() => this.populateBoard(2, 0)}>{this.state.board[2][0]}</td>
+                            <td onClick={() => this.populateBoard(2, 1)}>{this.state.board[2][1]}</td>
+                            <td onClick={() => this.populateBoard(2, 2)}>{this.state.board[2][2]}</td>
                         </tr>
                     </thead>
                 </table>
+                <h1>{this.state.winner}</h1>
             </div>
         )
     }
